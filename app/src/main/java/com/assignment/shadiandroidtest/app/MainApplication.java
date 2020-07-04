08b26c866b0fe6e2426d4e070c0e23e81c361292
Ashish <ashishm598@gmail.com>
2020-07-04 19:24:08 +0530

    [Added] Object Box in Application Class

--- app/src/main/java/com/assignment/shadiandroidtest/app/MainApplication.java
+++ app/src/main/java/com/assignment/shadiandroidtest/app/MainApplication.java
@@ -2,6 +2,13 @@ package com.assignment.shadiandroidtest.app;
 
 import android.app.Application;
 import android.content.Context;
+import android.util.Log;
+
+import com.assignment.shadiandroidtest.BuildConfig;
+import com.assignment.shadiandroidtest.entities.MyObjectBox;
+
+import io.objectbox.BoxStore;
+import io.objectbox.android.AndroidObjectBrowser;
 
 public class MainApplication extends Application {
 
@@ -9,14 +16,27 @@ public class MainApplication extends Application {
 
     public static final String LOG_TAG = "tm-tag";
 
+    private static BoxStore boxStore;
+
     @Override
     public void onCreate() {
         super.onCreate();
         appContext = this;
+
+        boxStore = MyObjectBox.builder().androidContext(this).build();
+
+        if (BuildConfig.DEBUG) {
+            boolean started = new AndroidObjectBrowser(boxStore).start(this);
+            Log.i("ObjectBrowser", "Started: " + started);
+        }
     }
 
     public static Context getAppContext() {
         return appContext;
+    }
+
+    public static BoxStore getBoxStore() {
+        return boxStore;
     }
 
 
